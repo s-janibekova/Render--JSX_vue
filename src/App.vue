6ac67ef9cb98c2c3848dd89443dacd272d25091a
S_Janybekova <s.t.janibekova@gmail.com>
2019-11-28 12:47:22 +0600

    SLots_JSX_Syntax_VUE

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    <div id="app">
+      <products-list :products="products"></products-list>
+      <products-list :products="products" :product-renderer="imageRenderer"></products-list>
+    </div>
+    <script src="vue.js"></script>
+    <script>
+      Vue.component('products-list', {
+        props: {
+          products: {
+            type: Array,
+            required: true
+          },
+          productRenderer: {  // <-- Here's our new prop
+            type: Function,
+            default (h, product) { // <-- By default just print the name
+              return product.name
+            }
+          }
+        },
+        render(h) {
+          return h('ul', [
+            this.products.map(product =>
+              h('li', [this.productRenderer(h, product)]) // use our new prop
+            )
+          ])
+        }
+      })
+      new Vue({
+        el: '#app',
+        data: {
+          products: [{
+            name: 'Magnifying Glass',
+            image: 'magnify.png'
+          }, {
+            name: 'Light Bulb',
+            image: 'bulb.png'
+          }],
+          imageRenderer(h, product) { // <-- The imageRenderer I'm passing in
+            return [
+              h('img', {
+                attrs: {
+                  src: product.image
+                }
+              }),
+              ' ',
+              product.name.toUpperCase()
+            ]
+          }
+        }
+      })
+    </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+window.Vue = Vue;
 
 Vue.config.productionTip = false
 
